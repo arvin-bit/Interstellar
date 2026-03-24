58fc6790e75a4d04ef4ce50407b61c797b76e9ae
arvin-bit <120641674+arvin-bit@users.noreply.github.com>
2026-03-24 09:10:48 +0100

    uwusup

--- config.js
+++ config.js
@@ -2,9 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    arvin: "uwubbg123",
-    loa: "hej",
-    oliver: "rip",
+    6a: "uwu",
   },
 };
 
